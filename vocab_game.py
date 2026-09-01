9782fc04b67ad6daee67e9f1352d2be0c199b2c9
414-25-chanakarn <54792@yupparaj.ac.th>
2026-09-01 10:18:55 +0700

    Update vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -101,4 +101,4 @@ if st.session_state.get("is_ended", False):
     show_result_dialog(ans1, ans2)
 
 st.divider()
-st.write("นางสาวณัฐชารี คงสุนทร เลขที่ 22 ม.4/12")
+st.write("นางสาวชนากานต์ กันะะมาโนช เลขที่ 25 ม.4/14")
